fa37a05c0d2d1b336e89410a16a3f3e35ab44690
Zaryab Zubair <zaryabz043@gmail.com>
2025-08-15 23:35:01 +0500

    [Day 1] loops 01

--- warmUp/loops01.js
+++ warmUp/loops01.js
@@ -3,6 +3,7 @@
 //* i < 5 => condition to be checked on each iteration
 //* i = i + 1 => increment on each iteration
 
+//? For Loop
 for (let i = 0; i < 5; i = i + 1) {
   console.log("Hello World");
 }
@@ -16,4 +17,13 @@ for (let i = 0; i < arr.length; i = i + 1) {
   if (value % 2 === 0) {
     console.log("Even Value", arr[i]);
   }
+}
+
+//? While Loop
+
+let i = 0;
+
+while (i < 5) {
+  console.log("Hi");
+  i = i + 1;
 }
