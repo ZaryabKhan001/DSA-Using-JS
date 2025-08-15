30ae34e60f8581c1bbe3099566fdbe05bdafef8f
Zaryab Zubair <zaryabz043@gmail.com>
2025-08-15 21:21:16 +0500

    [Day 1] programming 101

---
+++ warmUp/programming101.js
@@ -0,0 +1,31 @@
+console.log("hello world");
+
+//? Two ways of declaring variables in JS
+//* 1: let 2: const
+
+//? Difference between let and const?
+//* Variable declared using let can be changed but const variables are constants.
+
+let a = 10;
+let b = 5;
+console.log(a + b);
+
+//? Arrays
+//* Arrays are the collection of different values.
+
+const arr = [1, 2, 3, 4, 5];
+console.log("Array:", arr);
+
+//* Array also contain multiple data values
+
+const arr2 = ["Zaryab", 7, [3, 4, 5]];
+console.log(arr2);
+
+//? Objects
+//* Objects are key-value pairs.
+
+const obj = {
+  name: "Zaryab",
+  age: 30,
+};
+console.log(obj.name);
