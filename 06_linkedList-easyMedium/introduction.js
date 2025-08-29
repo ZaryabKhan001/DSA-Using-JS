b9b75713ab191e06c88c4b3463bbca7add6b9f0a
Zaryab Zubair <zaryabz043@gmail.com>
2025-08-29 21:15:02 +0500

    [Day 14] linkedlist-introduction

---
+++ 06_linkedList-easyMedium/introduction.js
@@ -0,0 +1,24 @@
+//? Linked List
+// A **Linked List** is a linear data structure in which elements (called **nodes**) are connected using pointers.
+// Each node contains:
+// - **Value** → the actual data
+// - **Reference/Pointer** → to the next node (and optionally to the previous node in **doubly linked lists**)
+
+//? Types of Linked Lists
+
+//? 1. Singly Linked List
+// - **value**: the data part
+// - **next**: a pointer/reference to the next node
+// - Moves only in **one direction (forward)**
+// - **Structure:**
+
+//? 2. Doubly Linked List
+// - **value**: the data
+// - **next**: pointer to the next node
+// - **prev**: pointer to the previous node
+// - Allows **bidirectional traversal**
+
+//? Key Terminologies
+// - **Head** → The first node of a linked list. It marks the entry point and points to the next node.
+// - **Tail** → The last node of a linked list. It points to `null` because there’s no node after it.
+// - **Linked List Representation** → Typically represented using its head node.
