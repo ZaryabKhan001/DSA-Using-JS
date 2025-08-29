bdf7008f76d10ee6edffafe264d0ddcfca84372e
Zaryab Zubair <zaryabz043@gmail.com>
2025-08-29 23:44:27 +0500

    [Day 14] addAtHead/Tail LinkedList

---
+++ 06_linkedList-easyMedium/03_addNodesToLinkedList.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  addAtHead(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.size++;
+  }
+  addAtTail(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let curr = this.head;
+      while (curr.next !== null) {
+        curr = curr.next;
+      }
+      curr.next = newNode;
+    }
+    this.size++;
+  }
+}
