736928161b9ded21f3b7798f9a02cae2dbdefd02
Zaryab Zubair <zaryabz043@gmail.com>
2025-09-01 21:08:35 +0500

    [Day 17] intersection-of-two-linkedLists

---
+++ 06_linkedList-easyMedium/08_intersectionOfTwoLinkedLists.js
@@ -0,0 +1,26 @@
+//? LeetCode #160
+//? Intersection of Two Linked Lists
+
+//? Approach:
+//* Traverse listA and place all elements in a Set,  then traverse list2.
+//* First matching element is intersection.
+
+var getIntersectionNode = function (headA, headB) {
+  let curr = headA;
+  let elements = new Set();
+  while (curr != null) {
+    elements.add(curr);
+    curr = curr.next;
+  }
+  curr = headB;
+  while (curr != null) {
+    if (elements.has(curr)) {
+      return curr;
+    }
+    curr = curr.next;
+  }
+  return null;
+};
+
+//? Time Complexity: O(m+n)
+//? Space Complexity: O(m)
