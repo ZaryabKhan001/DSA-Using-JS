2d597764c57b7a2b27ca28cb19619c23391d7002
Zaryab Zubair <zaryabz043@gmail.com>
2025-09-06 22:44:54 +0500

    [Day 22] revision-1 problem-3

---
+++ 07_revision_01/03_singleNumber.js
@@ -0,0 +1,23 @@
+//? LeetCode #136
+
+//* Single Number
+
+// Input: nums = [2,2,1]
+// Output: 1
+
+let nums = [2, 2, 1];
+
+var singleNumber = function (nums) {
+  let ans = 0;
+  let numsLength = nums.length;
+  for (let i = 0; i < numsLength; i = i + 1) {
+    ans = ans ^ nums[i];
+  }
+  return ans;
+};
+
+const answer = singleNumber(nums);
+console.log(answer);
+
+//? Time Complexity: O(n)
+//? Space Complexity: O(1)
