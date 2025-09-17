d4f66ec140766da1ccec78d4325e375843d4099f
Zaryab Zubair <zaryabz043@gmail.com>
2025-09-17 22:41:14 +0500

    [Day 31] introduction to stacks and queues

---
+++ 09_stack-and-queues/01_introduction.js
@@ -0,0 +1,59 @@
+//? What is a Stack?
+// Stack follows the LIFO (Last-In-First-Out) principle. The last element added is the first one to come out.
+
+//? Stack Operations
+// Push: Add an element to the top
+// Pop: Remove the top element
+// Peek/Top: View the top-most element
+
+//? Examples
+// Stacks of Books
+// Undo/Redo feature
+// Recursion (Function Call Stack)
+// Browsing History
+
+//? What is a Queue?
+// Queue follows the FIFO (First-In-First-Out) principle. The first element added is the first to be removed.
+
+//? Queue Operations
+// Enqueue: Add an element to the end
+// Dequeue: Remove element from the front
+// Peek/Front: View the front-most element
+
+//? Examples
+// Ticket Counter
+// OS Task Scheduling
+// Printers
+// Queues (in general)
+
+//? Why Use Stack and Queue❓
+// Organize data logically (based on problem’s need).
+// Order of Operations Matter
+// Optimize time & memory for specific scenarios
+
+//? Use Cases
+// Recursion (stack)
+// Level Order Traversal (queue)
+// BFS (queue)
+// DFS (stack)
+
+//? Implementation
+// let stack = [];
+// stack.push(10);
+// stack.push(7);
+// stack.pop();    // returns 7
+// stack.pop();    // returns 10
+
+//? Stack vs Queue
+
+//? 📥 Stack
+// Last-In-First-Out
+// Access only from top
+// Used in DFS, Recursion
+
+//? 📤 Queue
+// First-In-First-Out
+// Access from front & rear
+// Used in BFS, Scheduling
+// comparison with other Data-Structures
+// table
