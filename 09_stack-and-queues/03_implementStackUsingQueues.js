4d7b95ff836128ece8fc3e03fe4d86ae57c10dc0
Zaryab Zubair <zaryabz043@gmail.com>
2025-09-18 23:31:48 +0500

    [Day 32] implement-stack-using-queues

--- 09_stack-and-queues/03_implementStackUsingQueues.js
+++ 09_stack-and-queues/03_implementStackUsingQueues.js
@@ -28,7 +28,7 @@
 // myStack.pop(); // return 2
 // myStack.empty(); // return False
 
-//? Approach
+//? Approach 1
 // Initialize Two Queues.
 // Just add the element to q1. (q1.push(x))
 // Remove the last pushed element (i.e., rear of q1).
@@ -79,3 +79,48 @@ MyStack.prototype.empty = function () {
 // empty → O(1)
 
 //? Space Complexity: O(1)
+
+//? Approach 2:
+// We simulate LIFO (stack) using only FIFO (queue) operations.
+// We maintain a single queue (q).
+// Remove the last pushed element (i.e., rear of q1).
+// Insert it at the back (q.push(x)).
+// Rotate the queue to bring the new element to the front by dequeuing and enqueuing all previous elements.
+
+var MyStack = function () {
+  this.q = [];
+};
+
+MyStack.prototype.push = function (x) {
+  this.q.push(x);
+};
+
+MyStack.prototype.pop = function () {
+  let n = this.q.length;
+  for (let i = 0; i < n - 1; i++) {
+    this.q.push(this.q.shift());
+  }
+  return this.q.shift();
+};
+
+MyStack.prototype.top = function () {
+  let n = this.q.length;
+  for (let i = 0; i < n - 1; i++) {
+    this.q.push(this.q.shift());
+  }
+  let front = this.q.shift();
+  this.q.push(front);
+  return front;
+};
+
+MyStack.prototype.empty = function () {
+  return this.q.length === 0;
+};
+
+//? Time Complexity:
+// push → O(1)
+// pop/top → O(n)
+// empty → O(1)
+
+//? Space Complexity = O(n)
+//? Where n is the total number of elements in the stack.
