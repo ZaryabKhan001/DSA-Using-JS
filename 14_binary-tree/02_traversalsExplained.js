836e4b455310ee3b2d81d5e537f1263c02d29bf3
Zaryab Zubair <zaryabz043@gmail.com>
2025-10-21 23:03:30 +0500

    [Day 63] Trees traversals

---
+++ 14_binary-tree/02_traversalsExplained.js
@@ -0,0 +1,15 @@
+//? What is Tree Traversal?
+// Tree traversal is the process of visiting each node in a tree.
+
+//? Traversal Techniques:
+//? Pre-Order Traversal
+// Order: Root → Left → Right
+
+//? In-Order Traversal
+// Order: Left → Root → Right
+
+//? Post-Order Traversal
+// Order: Left → Right → Root
+
+//? Level-Order Traversal
+// Order: Traverse Level by Level (Top to Bottom)
