d007b3b73f79f0248a9a2100d1752e2e5bae14e3
Zaryab Zubair <zaryabz043@gmail.com>
2025-10-22 22:54:18 +0500

    [Day 64] postOrder traversal recursive

--- 14_binary-tree/03_treeTraversalsRecusive.js
+++ 14_binary-tree/03_treeTraversalsRecusive.js
@@ -73,3 +73,43 @@ var inorderTraversal = function (root) {
 
 //? Time Complexity = O(n)
 //? Space Complexity = O(n)
+
+//? LeetCode #145
+//? Binary Tree Postorder Traversal
+
+// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+//? Example 1:
+// Input: root = [1,null,2,3]
+// Output: [1,2,3]
+
+//? Example 2:
+// Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+// Output: [1,2,4,5,6,7,3,8,9]
+
+//? Approach
+// initialize an empty array ans to store the result.
+// Define a helper function traversal(curr):
+// If the current node is null, return.
+// Recursively traverse the left subtree.
+// Recursively traverse the right subtree.
+// Push the current node’s value into ans.
+// Call traversal(root) to start the traversal from the root.
+// Return the result array ans.
+
+var postorderTraversal = function (root) {
+  let ans = [];
+
+  function traversal(curr) {
+    if (!curr) return;
+    traversal(curr.left);
+    traversal(curr.right);
+    ans.push(curr.val);
+  }
+  traversal(root);
+
+  return ans;
+};
+
+//? Time Complexity = O(n)
+//? Space Complexity = O(n)
