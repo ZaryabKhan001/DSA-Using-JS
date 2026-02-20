d0c127df2cea34eb8d561338416fd01701f0a5a5
ZaryabKhan001 <zaryabz043@gmail.com>
2026-02-20 10:37:20 +0500

    [Day 140] min cost climbing stairs

---
+++ 23_dynamic-programming/04_minCostClimbingStairs.js
@@ -0,0 +1,53 @@
+//? LeetCode #746
+//? Min Cost Climbing Stairs
+
+// You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+// You can either start from the step with index 0, or the step with index 1.
+
+// Return the minimum cost to reach the top of the floor.
+
+//? Example 1:
+// Input: cost = [10,15,20]
+// Output: 15
+// Explanation: You will start at index 1.
+// - Pay 15 and climb two steps to reach the top.
+// The total cost is 15.
+
+//? Example 2:
+// Input: cost = [1,100,1,1,1,100,1,1,100,1]
+// Output: 6
+// Explanation: You will start at index 0.
+// - Pay 1 and climb two steps to reach index 2.
+// - Pay 1 and climb two steps to reach index 4.
+// - Pay 1 and climb two steps to reach index 6.
+// - Pay 1 and climb one step to reach index 7.
+// - Pay 1 and climb two steps to reach index 9.
+// - Pay 1 and climb one step to reach the top.
+// The total cost is 6.
+
+//? Approach:
+// Subproblem definition: Let dp[i] = minimum cost to reach step i.
+// Best Case: To stand before the staircase: dp[0] = 0, dp[1] = 0 (since you can start at step 0 or step 1 for free).
+// Transition relation
+// To reach step i, you could:
+// Come from i-1 → pay dp[i-1] + cost[i-1].
+// Come from i-2 → pay dp[i-2] + cost[i-2].
+// So, dp[i]=min(dp[i−1]+cost[i−1], dp[i−2]+cost[i−2]).
+// The result is dp[n] (minimum cost to step just beyond the last stair).
+
+//? Code:
+var minCostClimbingStairs = function (cost) {
+
+    const dp = [0, 0];
+
+    for (let i = 2; i <= cost.length; i = i + 1) {
+        dp[i] = Math.min((dp[i - 1] + cost[i - 1]), (dp[i - 2] + cost[i - 2]));
+    }
+
+    return dp[cost.length];
+
+};
+
+//? Time Complexity: O(n)
+//? Space Complexity: O(n)
