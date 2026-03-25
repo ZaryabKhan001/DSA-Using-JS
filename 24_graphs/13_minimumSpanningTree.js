e569d352baf67e98c294f4855288e0a516e48a8a
ZaryabKhan001 <zaryabz043@gmail.com>
2026-03-25 13:27:43 +0500

    [Day 161] minimum spanning tree

---
+++ 24_graphs/13_minimumSpanningTree.js
@@ -0,0 +1,24 @@
+//? Minimum Spanning Tree
+//? Spanning Tree
+// It is a subgraph that connects all vertuces of the graph with no cycles and exactly n-1 edges.
+
+//? Points to Remember:
+// All Nodes
+// Connected
+// No Cycles
+// (n-1) edges
+
+//? Why Minimum Spanning Tree?
+// Minimum edges to connect a network.
+// No Cycles.
+// Simplify the Structure.
+
+//? Use Case:
+// Network
+// Routing
+// Broadcasting
+// Infrastructure design (roads, pipes, electricity)
+
+//? Minimum Spanning Tree Algorithms
+// Prim's Algorithm
+// Kruskal's Algorithm
