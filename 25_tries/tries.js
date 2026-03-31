2d38a4919ae30152a455e957cfaade250b6f6ad1
ZaryabKhan001 <zaryabz043@gmail.com>
2026-03-31 12:38:57 +0500

    [Day 165] tries introduction

---
+++ 25_tries/tries.js
@@ -0,0 +1,16 @@
+//? Trie
+// Trie is a tree like data structure that is used to store strings efficiently, especially when prefix based operations are required. It is also known as Prefix-Tree.
+
+//? Specification of the Data Structure:
+// Words are stored character by character.
+// Common prefix are shared.
+// Each path represents a prefix.
+// Some of them are marked as words.
+
+//? What is the purpose of using a Trie data structure?
+// Determine the number of words that begin with ‘ca’.
+// [car, cat, cap, cup, cupboard, cut, cutter, cot]
+
+//? Advantage’s of Trie Data Structure
+// Prefix-based operations: It efficiently supports prefix searches, making it ideal for autocomplete and search suggestion systems.
+// Efficient searching: Trie allows fast search, insertion, and deletion operations in O(L) time, where L is the length of the word.
